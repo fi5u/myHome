2c6e14e6d58f2dd6e4ff24e20abfb5f321d1c162
Tommy Fisher <tommybfisher@gmail.com>
2014-12-04 09:19:49 +0200

    isAvailable successfully filtered

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,28 +1,37 @@
 (function () {
     var app = angular.module('myhome', []);
 
-    app.controller('MyHomeController', ['$http', function($http) {
+    app.controller('MyHomeController', function() {
+
+    });
+
+    app.controller('SearchController', ['$scope', function($scope) {
+        $scope.params = {
+            isAvailable: true,
+            orderby: 'rentalCost',
+            reverseOrder: false,
+            hasBalcony: '',
+            hasGarden: '',
+            allowsPets: '',
+            hasSauna: ''
+        };
+    }]);
+
+    app.controller('SearchResultsController', ['$scope', '$http', 'resultsFilter', function($scope, $http, resultsFilter) {
         var search = this;
         search.homes = [];
 
         $http.get('assets/js/homes.json').success(function(data) {
             search.homes = data;
+            search.filteredHomes = resultsFilter(search.homes, $scope.params);
         });
+
+        $scope.$watch('params', function (newParams) {
+            if (newParams) {
+                search.filteredHomes = resultsFilter(search.homes, newParams);
+            }
+        }, true); // true needed for watching objects
     }]);
-
-    app.controller('SearchController', function() {
-        this.isAvailable = true;
-        this.orderby = 'rentalCost';
-        this.reverseOrder = false;
-        this.hasBalcony = '';
-        this.hasGarden = '';
-        this.allowsPets = '';
-        this.hasSauna = '';
-
-        this.toggleReverseOrder = function() {
-            this.reverseOrder != this.reverseOrder;
-        };
-    });
 
     app.directive('searchInputBlock', function() {
         return {
@@ -105,6 +114,35 @@
         return function(input) {
             return input.replace(' ', '');
         };
+    });
+
+    /**
+     * Iterate through the results, passing back only what is searched for
+     * @return {object} Filtered results
+     */
+    app.filter('results', function() {
+        return function(input, params) {
+                // Array to store keys to be removed
+            var toRemove = [],
+                // Array to store filtered results
+                filteredResults = [];
+
+            // Loop through results and place keys to remove on toRemove[]
+            for (var i = 0; i < input.length; i++) {
+                if (((params.isAvailable === 'true' || params.isAvailable === true) && input[i].available === false) ||
+                    ((params.isAvailable === 'false' || params.isAvailable === false) && input[i].available === true)) {
+                    toRemove.push(i);
+                }
+            };
+
+            // Loop through results adding to filteredResults[] only if not in toRemove[]
+            for (var i = 0; i < input.length; i++) {
+                if (toRemove.indexOf(i) === -1) {
+                    filteredResults.push(input[i]);
+                }
+            };
+            return filteredResults;
+        }
     });
 
 })();
