8c2d1a27132a570f486f2e77626701f4494c639f
Tommy Fisher <tommybfisher@gmail.com>
2014-12-04 11:04:16 +0200

    Removed unused code

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -49,45 +49,7 @@
     app.directive('singleResult', function() {
         return {
             restrict: 'E',
-            templateUrl: 'single-result.html',
-            controller: function() {
-                this.resultShow = function(searchCtrl, home) {
-
-                    // Check through each property, if there's a mismatch remove from DOM straight away and don't continue
-
-                    // Store extra properties on an array to loop through
-                    // [0] = searchCtrl, [1] = home.extras[0]
-                    var extrasProp = [
-                        ['hasBalcony', 'balcony'],
-                        ['hasGarden', 'garden'],
-                        ['allowsPets', 'pets'],
-                        ['hasSauna', 'sauna']
-                    ];
-
-                    if ((searchCtrl.isAvailable === 'true' || searchCtrl.isAvailable === true) || (searchCtrl.isAvailable === 'false' || searchCtrl.isAvailable === false)) {
-                        if (((searchCtrl.isAvailable === 'true' || searchCtrl.isAvailable === true) && home.available === false) || ((searchCtrl.isAvailable === 'false' || searchCtrl.isAvailable === false) && home.available === true)) {
-                            return false;
-                        }
-                    }
-
-                    // Loop through the extrasProp array to remove elements that do not match the search
-                    for (var i = 0; i < extrasProp.length; i++) {
-                        if (searchCtrl[extrasProp[i][0]] === 'true' || searchCtrl[extrasProp[i][0]] === 'false') {
-                            if ((searchCtrl[extrasProp[i][0]] === 'true' && home.extras[0][extrasProp[i][1]] === false) || (searchCtrl[extrasProp[i][0]] === 'false' && home.extras[0][extrasProp[i][1]] === true)) {
-                                return false;
-                            }
-                        }
-                    };
-
-                    // Otherwise return true
-                    return true;
-                };
-
-                this.hasExtra = function(hasExtra) {
-                    return hasExtra;
-                };
-            },
-            controllerAs: 'singleResCtrl'
+            templateUrl: 'single-result.html'
         }
     });
 
@@ -161,7 +123,7 @@
                 }
             };
             return filteredResults;
-        }
+        };
     });
 
 })();
