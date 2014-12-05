eab862709e8d61dc1b418b0972a49b07363e2347
Tommy Fisher <tommybfisher@gmail.com>
2014-12-05 11:16:47 +0200

    Styling to gallery scroll

---
+++ app/assets/js/script.js
@@ -0,0 +1,19 @@
+(function($, window) {
+    /*var getThumbScrollerH = function() {
+        return $('.gallery__thumb-scroller').outerHeight();
+    }
+
+    var scrollThumbScroller = function(dir) {
+        $('.gallery__thumb-scroller').animate({
+            top: '300px'
+        }, 500);
+    };
+
+    $(window).load(function() {
+        console.log(getThumbScrollerH());
+    });
+
+    $('.gallery__thumb-scroll--down').click(function() {
+        scrollThumbScroller('up');
+    });*/
+})(jQuery, window);
