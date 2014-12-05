9b6f4a3ce982a5780724f7fac7a4664a946eead2
Tommy Fisher <tommybfisher@gmail.com>
2014-12-05 11:17:30 +0200

    Removed references to thumb scroller

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,19 +1,3 @@
 (function($, window) {
-    /*var getThumbScrollerH = function() {
-        return $('.gallery__thumb-scroller').outerHeight();
-    }
 
-    var scrollThumbScroller = function(dir) {
-        $('.gallery__thumb-scroller').animate({
-            top: '300px'
-        }, 500);
-    };
-
-    $(window).load(function() {
-        console.log(getThumbScrollerH());
-    });
-
-    $('.gallery__thumb-scroll--down').click(function() {
-        scrollThumbScroller('up');
-    });*/
 })(jQuery, window);
