ed9966838ffbaf2a0b8cee0267ea01ec37976b4a
Tommy Fisher <tommybfisher@gmail.com>
2014-12-17 08:10:39 +0200

    Single info window per time

--- app/app/components/search/controllers/MapViewController.js
+++ app/app/components/search/controllers/MapViewController.js
@@ -1,8 +1,15 @@
 myHomeApp.controller('MapViewController', ['$scope', 'uiGmapGoogleMapApi', function($scope, uiGmapGoogleMapApi) {
     $scope.map = {};
+    $scope.activeMarker = {};
+    $scope.showInfo = false;
 
-    $scope.setBounds = function() {
+    $scope.clearActiveMarker = function() {
+        $scope.activeMarker = {};
+    };
 
+    $scope.setActiveMarker = function(marker) {
+        $scope.activeMarker = {};
+        $scope.activeMarker = marker;
     };
 
     uiGmapGoogleMapApi.then(function(maps) {
@@ -10,23 +17,41 @@ myHomeApp.controller('MapViewController', ['$scope', 'uiGmapGoogleMapApi', funct
             center: $scope.$storage.params.views.map.location,
             zoom: $scope.$storage.params.views.map.zoom,
             events: {
-               zoom_changed: function(map) {
-                    $scope.$storage.params.views.map.zoom = map.getZoom();
+                tilesloaded: function (map) {
+                    $scope.$apply(function () {
+                        $scope.mapInstance = map;
+                    });
                 },
+                zoom_changed: function(map) {
+                    $scope.$storage.params.views.map.zoom = map.getZoom();
+                }/*,
                 click: function() {
-                    var bounds = new google.maps.LatLngBounds();
+                    var bounds = new maps.LatLngBounds();
                     for (var i in $scope.filteredHomes) {
                         bounds.extend($scope.filteredHomes[i].location);
                     }
-                    $scope.map.fitBounds(bounds);
-                }
+                    //$scope.mapInstance.fitBounds(bounds);
+                }*/
             },
-            bounds: {}
-        };
-        $scope.options = {
-            scrollwheel: true
+            fit: true
         };
 
+        $scope.options = {
+            scrollwheel: true,
+            maxZoom: 17
+        };
+
+        $scope.windowOptions = {
+            maxWidth: 600,
+            visible: true
+        };
+
+        _.each($scope.filteredHomes, function(marker) {
+            marker.markerClick = function() {
+                $scope.showInfo = true;
+                $scope.setActiveMarker(marker);
+            };
+        });
     });
 
     $scope.resetBounds = function() {
