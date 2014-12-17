4a109637cbb7d44ecaedfa153e95f24f2bfb27c3
Tommy Fisher <tommybfisher@gmail.com>
2014-12-17 13:22:44 +0200

    Single view style tweaks

--- app/app/components/search/controllers/TabBarController.js
+++ app/app/components/search/controllers/TabBarController.js
@@ -3,6 +3,6 @@ myHomeApp.controller('TabBarController', ['$scope', function($scope) {
     $scope.changeTab = function(newTab) {
         $scope.$storage.params.views.last = newTab;
         $scope.$storage.paramsInit.views.last = newTab;
-    }
+    };
 
 }]);
