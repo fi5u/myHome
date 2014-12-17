5a927c022372732a1714e78d4e2dd2dae911b972
Tommy Fisher <tommybfisher@gmail.com>
2014-12-17 12:50:51 +0200

    Reset stays on same view

--- dist/app/components/search/controllers/TabBarController.js
+++ dist/app/components/search/controllers/TabBarController.js
@@ -2,6 +2,7 @@ myHomeApp.controller('TabBarController', ['$scope', function($scope) {
 
     $scope.changeTab = function(newTab) {
         $scope.$storage.params.views.last = newTab;
+        $scope.$storage.paramsInit.views.last = newTab;
     }
 
 }]);
